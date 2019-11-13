753e85104dd4e7a70127bd94b1cf13df4185cee9
Doran Kayoumi <dorankayoumi@gmail.com>
2019-11-13 10:47:24 +0100

    Add inital Hanoi class

---
+++ src/hanoi/Hanoi.java
@@ -0,0 +1,21 @@
+package hanoi;
+
+public class Hanoi {
+    private int moves;
+
+    public Hanoi(int moves) {
+        this.moves = moves;
+    }
+
+    public void solve() {
+    }
+
+    public int[][] status() {
+    }
+
+    public boolean finished() {
+    }
+
+    public int turn {
+    }
+}
