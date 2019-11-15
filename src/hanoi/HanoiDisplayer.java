d1ce8763f0f7cbbf8bc3cbf63917a82e4d690ddc
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2019-11-15 10:24:04 +0100

    refactor

--- src/hanoi/HanoiDisplayer.java
+++ src/hanoi/HanoiDisplayer.java
@@ -2,14 +2,14 @@ package hanoi;
 
 public class HanoiDisplayer {
 
-    private String[] conversionTable = {"One  ", "Two  ", "Three"};
+    private String[] needleNames = {"One  ", "Two  ", "Three"};
 
     public void display(Hanoi hanoi) {
         System.out.println("-- Turn: " + hanoi.turn());
         int[][] status = hanoi.status();
 
         for(int row = 0; row < status.length; ++row){
-            System.out.print(conversionTable[row] + " : [");
+            System.out.print(needleNames[row] + " : [");
             for(int col = 0; col < status[row].length; ++col){
                 System.out.print(" <" + status[row][col] +">");
             }
